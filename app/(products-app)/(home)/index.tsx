7f781527411533f2c2c73589e9e9352e1c59a7b9
Jeral Pineda <jeral.cris@gmail.com>
2024-12-17 21:50:02 -0600

    fix: configure fonts and colors

--- app/(products-app)/(home)/index.tsx
+++ app/(products-app)/(home)/index.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+
+import { ThemedText } from "@/presentation/theme/components/ThemedText";
+import { useThemeColor } from "@/presentation/theme/hooks/useThemeColor";
 
 export default function Home() {
+  const primary = useThemeColor({}, "primary");
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ paddingTop: 100, paddingHorizontal: 20 }}>
+      <ThemedText style={{ fontFamily: "KanitBold", color: primary }}>
+        Home
+      </ThemedText>
+      <ThemedText style={{ fontFamily: "KanitRegular" }}>Home</ThemedText>
+      <ThemedText style={{ fontFamily: "KanitThin" }}>Home</ThemedText>
     </View>
   );
 }
