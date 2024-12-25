eec3b0bdc402700a7fa4727d8ee74c349d1fe2fb
Jeral Pineda <jeral.cris@gmail.com>
2024-12-25 09:31:38 -0600

    fix: add scroll infinite

--- presentation/products/components/product-list.tsx
+++ presentation/products/components/product-list.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import { FlatList, RefreshControl } from "react-native";
+
 import { Product } from "@/core/products/interface/product.interface";
-import { FlatList, Text, View } from "react-native";
 import { ProductCard } from "./product-card";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface ProductListProps {
   products: Product[];
@@ -11,13 +14,32 @@ export default function ProductList({
   products,
   loadNextPage,
 }: ProductListProps) {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const queryClient = useQueryClient();
+
+  const onPullToRefresh = async () => {
+    setIsRefreshing(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    queryClient.invalidateQueries({
+      queryKey: ["products", "infinite"],
+    });
+
+    setIsRefreshing(false);
+  };
+
   return (
     <FlatList
       data={products}
       numColumns={2}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <ProductCard product={item} />}
+      onEndReached={loadNextPage}
+      onEndReachedThreshold={0.8}
       showsVerticalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />
+      }
     />
   );
 }
