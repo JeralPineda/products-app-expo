24823d12d20792c00c65538008badaad883c4413
Jeral Pineda <jeral.cris@gmail.com>
2024-12-29 20:10:14 -0600

    fix: invalidar cache

--- presentation/products/hooks/usePrdouct.ts
+++ presentation/products/hooks/usePrdouct.ts
@@ -1,21 +1,38 @@
 import { Alert } from "react-native";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { getProductById } from "@/core/products/actions/get-product-by-id.action";
 import { Product } from "@/core/products/interface/product.interface";
 import { updateCreateProduct } from "@/core/products/actions/create-update-product.action";
+import { useRef } from "react";
 
 export function useProduct(productId: string) {
+  const productIdRef = useRef(productId); // "new" or UUID
+  const queryClient = useQueryClient();
+
   const productQuery = useQuery({
-    queryKey: ["product", productId],
+    queryKey: ["products", productId],
     queryFn: () => getProductById(productId),
     staleTime: 1000 * 60 * 60, // 1 hour
   });
 
   const productMutation = useMutation({
-    mutationFn: async (data: Product) => updateCreateProduct(data),
+    mutationFn: async (data: Product) =>
+      updateCreateProduct({
+        ...data,
+        id: productIdRef.current,
+      }),
+
     onSuccess: (data: Product) => {
-      // TODO: invalidar el cache del producto
+      productIdRef.current = data.id;
+
+      queryClient.invalidateQueries({
+        queryKey: ["products", "infinite"],
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ["products", data.id],
+      });
 
       Alert.alert("Producto guardado", "El producto se ha guardado");
     },
